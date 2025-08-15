1fa58456c2a35fb9b9e01c3e66cddbc2527e0264
a34725173-hash <a34725173@gmail.com>
2025-08-15 16:14:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,53 +4,68 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-let games = {}; // roomId: { players: [socketId...], choices: {}, etc. }
+let games = {};
 
 function createOrJoinRoom(socket) {
-  // 嘗試配對未滿2人的房間
   for (const [room, info] of Object.entries(games)) {
     if (info.players.length < 2) {
       info.players.push(socket.id);
       return room;
     }
   }
-  // 新開一房
-  let rid = 'room-'+Math.random().toString(36).substring(2,8);
+  let rid = 'room-' + Math.random().toString(36).substring(2, 8);
   games[rid] = { players: [socket.id], choices: {}, names: {} };
   return rid;
 }
 
-io.on('connection', (socket) => {
+function determineRoles(room) {
+  let dice1 = Math.floor(Math.random()*6)+1 + Math.floor(Math.random()*6)+1;
+  let dice2 = Math.floor(Math.random()*6)+1 + Math.floor(Math.random()*6)+1;
+  let players = games[room].players;
+  let assign = {};
+  if (dice1 > dice2) {
+    assign[players[0]] = 'atk';
+    assign[players[1]] = 'def';
+  } else if (dice2 > dice1) {
+    assign[players] = 'def';
+    assign[players[1]] = 'atk';
+  } else {
+    assign[players] = 'atk';
+    assign[players[1]] = 'def';
+  }
+  io.to(players).emit('roleAssign', assign[players]);
+  io.to(players[1]).emit('roleAssign', assign[players[1]]);
+  io.to(room).emit('newRound', { dice1, dice2 });
+}
+
+io.on('connection', socket => {
   let playerRoom = createOrJoinRoom(socket);
   socket.join(playerRoom);
 
   socket.on('setName', (name) => {
     games[playerRoom].names[socket.id] = name;
     io.to(playerRoom).emit('players', Object.values(games[playerRoom].names));
+    if (games[playerRoom].players.length === 2) determineRoles(playerRoom);
   });
 
   socket.on('choice', (data) => {
     games[playerRoom].choices[socket.id] = data;
-    // 當2人都完成選擇
     if (Object.keys(games[playerRoom].choices).length === 2) {
-      // 廣播給全房
       io.to(playerRoom).emit('battle', games[playerRoom].choices);
-      // 重設choices
       games[playerRoom].choices = {};
+      determineRoles(playerRoom); // 新回合自動決定攻防
     }
   });
 
   socket.on('disconnect', () => {
-    // 清理房間和玩家
-    if (games[playerRoom]) {
-      games[playerRoom].players = games[playerRoom].players.filter(id=>id!=socket.id);
+    if(games[playerRoom]){
+      games[playerRoom].players = games[playerRoom].players.filter(id=>id!==socket.id);
       delete games[playerRoom].names[socket.id];
-      if (games[playerRoom].players.length === 0) delete games[playerRoom];
+      if(games[playerRoom].players.length===0) delete games[playerRoom];
       else io.to(playerRoom).emit('players', Object.values(games[playerRoom].names));
     }
   });
