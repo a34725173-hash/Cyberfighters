ef22f07ffc00231ac56313b6a7bcf68ad3043391
a34725173-hash <a34725173@gmail.com>
2025-08-15 16:14:00 +0800

    Update network.js

--- public/network.js
+++ public/network.js
@@ -1,9 +1,11 @@
-window.Network = (function(){
+window.Network = (function () {
   const socket = io();
   return {
-    setName: (name)=>socket.emit("setName", name),
-    sendChoice: (data)=>socket.emit("choice", data),
-    onPlayers: (cb)=>socket.on("players", cb),
-    onBattle: (cb)=>socket.on("battle", cb)
+    setName: (name) => socket.emit("setName", name),
+    sendChoice: (data) => socket.emit("choice", data),
+    onPlayers: (cb) => socket.on("players", cb),
+    onBattle: (cb) => socket.on("battle", cb),
+    onRoleAssign: (cb) => socket.on("roleAssign", cb),
+    onNewRound: (cb) => socket.on("newRound", cb)
   };
 })();
